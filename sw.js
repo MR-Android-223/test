43c0ea111dc015dcf0f765676916616b275b0a68
Mohammad Wassouf <omarhweh619@gmail.com>
2026-03-13 02:26:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,20 @@
-const CACHE_NAME = 'vault-cache-v4';
-
+const CACHE_NAME = 'note-v2';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './icon.png',
-  './manifest.json'
+  './icon.png'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
